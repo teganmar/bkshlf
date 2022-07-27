0aefbef21a4157f2f7c61df638e84e0b1972e73f
Tegan Marianchuk <tmarianchuk@ELIGO-MAC27.local>
2022-07-27 10:40:01 -0500

    changed main to bind to keroku port & host

--- main.py
+++ main.py
@@ -283,4 +283,5 @@ def del_by_title(title:str):
     return f"{title} was deleted from the database"
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='127.0.0.1', port=8301,reload=True)
+    #uvicorn.run("main:app", host='127.0.0.1', port=8301,reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=5000,reload=True, debug=True)
